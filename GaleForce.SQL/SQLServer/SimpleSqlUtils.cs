89dbf989cbb4bc9389f632f6c4815f31b41b6dc0
Jim Gale <jimgaleforce@gmail.com>
2022-10-26 11:08:05 -0700

    added ExecuteBulkCopy for inserts - 1.0.5

--- GaleForce.SQL/SQLServer/SimpleSqlUtils.cs
+++ GaleForce.SQL/SQLServer/SimpleSqlUtils.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using GaleForceCore.Builders;
+using GaleForceCore.Helpers;
 
 namespace GaleForce.SQL.SQLServer
 {
@@ -61,6 +66,111 @@ namespace GaleForce.SQL.SQLServer
         {
             var sql = ssBuilder.Build(source);
             return Utils.SqlExecute(sql, connection);
+        }
+
+        /// <summary>
+        /// Executes the a bulk copy on insert fields.
+        /// </summary>
+        /// <typeparam name="TRecord">The type of the t record.</typeparam>
+        /// <param name="ssBuilder">The ss builder.</param>
+        /// <param name="source">The source dataset/records.</param>
+        /// <param name="connection">The connection.</param>
+        /// <param name="bulkSize">Size of the bulk.</param>
+        /// <param name="retries">The retries.</param>
+        /// <param name="timeoutInSeconds">The timeout in seconds (per copy).</param>
+        /// <returns>System.Int32.</returns>
+        public static async Task<int> ExecuteBulkCopy<TRecord>(
+            this SimpleSqlBuilder<TRecord> ssBuilder,
+            IEnumerable<TRecord> source,
+            string connection,
+            int bulkSize = 50000,
+            int retries = 3,
+            int timeoutInSeconds = 600)
+        {
+            var sql = ssBuilder.Build(source);
+            var fields = ssBuilder.Inserts;
+            var type = typeof(TRecord);
+            var props = type.GetProperties();
+
+            var dt = new DataTable();
+            var fieldProps = new List<PropertyInfo>();
+
+            var hasValues = ssBuilder.Valueset.Count() > 0;
+            var values = hasValues ? ssBuilder.ValueExpressions.Select(v => v.Compile()).ToList() : null;
+
+            foreach (var originalField in fields)
+            {
+                var field = originalField.Contains(".")
+                    ? originalField.Substring(originalField.IndexOf(".") + 1)
+                    : originalField;
+                var property = props.FirstOrDefault(p => p.Name == field);
+                if (property != null)
+                {
+                    Type propertyType = property.PropertyType;
+                    if (propertyType.IsGenericType &&
+                        propertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                    {
+                        propertyType = Nullable.GetUnderlyingType(propertyType);
+                    }
+
+                    dt.Columns.Add(new DataColumn(property.Name, propertyType));
+                    fieldProps.Add(property);
+                }
+            }
+
+            var groupedData = source.ToArray().Split(bulkSize);
+
+            var count = 0;
+            using (var destConn = new SqlConnection(connection))
+            {
+                destConn.Open();
+                var bulkCopy = new SqlBulkCopy(destConn)
+                {
+                    DestinationTableName = ssBuilder.TableName,
+                    BulkCopyTimeout = timeoutInSeconds
+                };
+
+                foreach (var group in groupedData)
+                {
+                    dt.Rows.Clear();
+                    foreach (var record in group)
+                    {
+                        var columns = new List<object>();
+                        var fieldIndex = 0;
+                        foreach (var fp in fieldProps)
+                        {
+                            var value = (hasValues ? values[fieldIndex].Invoke(record) : fp.GetValue(record, null)) ??
+                                DBNull.Value;
+                            columns.Add(value);
+                        }
+
+                        dt.Rows.Add(columns.ToArray());
+                    }
+
+                    var retryIndex = 0;
+                    while (retryIndex < retries)
+                    {
+                        try
+                        {
+                            await bulkCopy.WriteToServerAsync(dt);
+                            break;
+                        }
+                        catch
+                        {
+                            if (++retryIndex >= retries)
+                            {
+                                throw;
+                            }
+                        }
+                    }
+
+                    count += dt.Rows.Count;
+                }
+
+                destConn.Close();
+            }
+
+            return count;
         }
     }
 }
--- TestGaleForce.SQL/UnitTest1.cs
+++ TestGaleForce.SQL/UnitTest1.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using System.Data;
 using GaleForce.SQL.SQLServer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -48,10 +48,76 @@ namespace TestGaleForce.SQL
         {
             Assert.AreEqual(true, TestValue<bool>(true), "Bool not interpreted properly");
         }
+
+        // [TestMethod]
+        // public void TestLocalSqlBulkCopy()
+        // {
+        // SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        // builder.DataSource = "MS-BOSS-JIMGALE\\ML";
+        // builder.InitialCatalog = "LOCALTEST";
+        // builder.IntegratedSecurity = true;
+
+        // var connection = builder.ConnectionString;
+
+        // var data = LocalTableRecord.GetData();
+        // var sql = new SimpleSqlBuilder<LocalTableRecord>(LocalTableRecord.TableName)
+        // .Insert(l => l.Id, l => l.Str1, l => l.Int1)
+        // .ExecuteBulkCopy(data, connection);
+        // }
     }
 
     public class TestRecord<T>
     {
         public T Value { get; set; }
+    }
+
+    public class LocalTableRecord
+    {
+        public const string TableName = "LocalTable";
+
+        public int Id { get; set; }
+
+        public string Str1 { get; set; }
+
+        public int? Int1 { get; set; }
+
+        public bool? Bool1 { get; set; }
+
+        public string Str2 { get; set; }
+
+        public int? Int2 { get; set; }
+
+        public static List<LocalTableRecord> GetData()
+        {
+            var recs = new List<LocalTableRecord>();
+            recs.Add(
+                new LocalTableRecord
+                {
+                    Id = 1,
+                    Str1 = "Str1a",
+                    Int1 = 101,
+                    Bool1 = true,
+                    Str2 = "Str2a"
+                });
+            recs.Add(
+                new LocalTableRecord
+                {
+                    Id = 2,
+                    Str1 = "Str1b",
+                    Int1 = 102,
+                    Bool1 = false,
+                    Str2 = "Str2b"
+                });
+            recs.Add(
+                new LocalTableRecord
+                {
+                    Id = 3,
+                    Str1 = "Str1c",
+                    Int1 = 103,
+                    Bool1 = true,
+                    Str2 = "Str2c"
+                });
+            return recs;
+        }
     }
 }
